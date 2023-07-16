be79094e928454289bf05c8c06164a18f8b468bc
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2023-07-16 09:40:42 +0200

    ready for publish

--- FuseSearch/Models.cs
+++ FuseSearch/Models.cs
@@ -1,21 +1,10 @@
 namespace FuseSearch;
 
-internal struct ScoreInfo
-{
-    public required int Errors { get; init; }
-    public required int CurrentLocation { get; init; }
-    public required int ExpectedLocation { get; init; }
-    public required int Distance { get; init; }
-    public required bool IgnoreLocation { get; init; }
-}
+internal record struct ScoreInfo(int Errors, int CurrentLocation, int ExpectedLocation, int Distance,
+    bool IgnoreLocation);
 
 internal record struct InternalSearchResult(bool IsMatch, double Score);
+
 internal record struct Index(List<Chunk> Chunks, string Pattern);
 
-internal struct Chunk
-{
-    public required int StartIndex { get; init; }
-    public required string Pattern { get; init; }
-    public required Dictionary<char, int> Alphabet { get; init; }
-}
-
+internal record struct Chunk(int StartIndex, string Pattern, Dictionary<char, int> Alphabet);
